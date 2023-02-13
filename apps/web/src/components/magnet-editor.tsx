fb91759c959947c8cb2e2835cd43cb4409f52e5d
kysley <evan@kysley.com>
2023-02-12 23:05:07 -0500

    add editor & scale slider

--- apps/web/src/components/magnet-editor.tsx
+++ apps/web/src/components/magnet-editor.tsx
@@ -1,18 +1,33 @@
 import { useSelectedMagnet } from "../hooks/use-selected-magnet";
 import * as Slider from "@radix-ui/react-slider";
+import "./magnet-editor.styles.css";
 
 export function MagnetEditor() {
   const m = useSelectedMagnet();
-  if (!m) return null;
+  if (!m.magnet.id) return null;
 
   return (
     <div>
       Scale
-      <Slider.Root defaultValue={[50]} max={100} step={1} aria-label="Volume">
-        <Slider.Track>
-          <Slider.Range />
+      <Slider.Root
+        defaultValue={[1]}
+        max={2}
+        min={0}
+        step={0.1}
+        aria-label="Volume"
+        className="slider-root"
+        onValueChange={(value) => {
+          console.log(value[0]);
+          m.setMagnet((prev) => ({
+            ...prev,
+            style: { ...prev.style, transform: `scale(${value[0]})` },
+          }));
+        }}
+      >
+        <Slider.Track className="slider-track">
+          <Slider.Range className="slider-range" />
         </Slider.Track>
-        <Slider.Thumb />
+        <Slider.Thumb className="slider-thumb" />
       </Slider.Root>
     </div>
   );
