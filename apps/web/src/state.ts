39c4159008b4deaa9c3b07349b6fffaa39763640
kysley <evan@kysley.com>
2023-02-12 23:04:44 -0500

    add Magnet type

--- apps/web/src/state.ts
+++ apps/web/src/state.ts
@@ -1,7 +1,16 @@
 import { atom } from "jotai";
 import { atomFamily } from "jotai/utils";
+import { CSSProperties } from "react";
 
-export const magnetFamily = atomFamily((id: string) => atom({ id }));
+export type Magnet = {
+  id: string;
+  x: number;
+  y: number;
+  url: string;
+  style?: CSSProperties;
+};
+
+export const magnetFamily = atomFamily((id: string) => atom<Magnet>({ id }));
 export const activeMagnet = atom("");
 export const selectedMagnetIdAtom = atom<string | null>(null);
 export const quickMenuAtom = atom<"PHOTO" | "IMAGE" | null>(null);
