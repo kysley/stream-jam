1be00e9873f42bbc1466e813712b1f0a64af80e1
kysley <evan@kysley.com>
2023-02-25 00:05:45 -0500

    clean up

--- apps/web/src/components/stream-preview.tsx
+++ apps/web/src/components/stream-preview.tsx
@@ -1,18 +1,11 @@
 export function StreamPreview() {
   return (
-    <div
-      style={{
-        justifySelf: "center",
-        display: "flex",
-        // transform: "scale(0.6)",
-      }}
-    >
-      <iframe
-        height={1080 * 0.6}
-        width={1920 * 0.6}
-        className="stream-preview"
-        src="https://player.twitch.tv/?channel=moonmoon&parent=localhost&muted=true&autoplay=false"
-      />
-    </div>
+    <iframe
+      height={1080}
+      width={1920}
+      style={{ border: "none" }}
+      className="stream-preview"
+      src="https://player.twitch.tv/?channel=moonmoon&parent=localhost&muted=true"
+    />
   );
 }
