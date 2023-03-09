18e1e9b13e2f921dfabd3f7953bdb6a555ee0ace
kysley <evan@kysley.com>
2023-03-09 01:17:18 -0500

    input & color tweaks

--- apps/web/src/components/input/input.css.ts
+++ apps/web/src/components/input/input.css.ts
@@ -8,34 +8,35 @@ export const input = recipe({
     paddingLeft: "6px",
     fontSize: "15px",
     lineHeight: "32px",
-    border: `1px solid transparent`,
+    border: "1px solid transparent",
     transition: "background-color, border-color ease-in .035s",
     width: "100%",
-    backgroundColor: vars.colors.gray3,
-    ":focus": {
-      outline: `1px dashed ${vars.colors.gray7}`,
-      backgroundColor: vars.colors.gray5,
-    },
-    ":hover": {
-      backgroundColor: vars.colors.gray5,
-      borderColor: vars.colors.gray8,
-    },
   },
   variants: {
     intent: {
       neutral: {
         borderColor: vars.colors.gray7,
         backgroundColor: vars.colors.gray3,
+        color: vars.colors.gray12,
+        ":focus": {
+          outline: `dotted thin ${vars.colors.gray7}`,
+          backgroundColor: vars.colors.gray5,
+        },
+        ":hover": {
+          backgroundColor: vars.colors.gray5,
+          borderColor: vars.colors.gray8,
+        },
       },
       danger: {
         borderColor: vars.colors.red7,
         backgroundColor: vars.colors.red3,
+        color: vars.colors.red12,
         ":hover": {
           backgroundColor: vars.colors.red5,
           borderColor: vars.colors.red8,
         },
         ":focus": {
-          outline: `1px dashed ${vars.colors.red7}`,
+          outline: `dotted thin ${vars.colors.red7}`,
           backgroundColor: vars.colors.red5,
         },
       },
